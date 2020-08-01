44ffec6324902019d0fc24807a0f1fe496d797c8
scotsz <xz1qwe@gmail.com>
2020-08-01 08:50:48 +0500

    remove interrupt

--- main.c
+++ main.c
@@ -14,20 +14,6 @@
 #define DELAY_IN 50 // 1/20000000*256*256*4*50=0.66sec
 #define DELAY_SIGNAL 100
 
-unsigned counters[10];
-int signal_flag = 0;
-int signal_timer = 0;
-
-void __interrupt() timer0(void) {
-    for (int i = 0; i < 10; i++) {
-        counters[i]++;
-    }
-    if (signal_flag) {
-        signal_timer++;
-    }
-    T0IF = 0;
-}
-
 void init() {
     TRISA = ~0x01; // 0 is out rest is in
     TRISB = 0xFF;
@@ -37,9 +23,6 @@ void init() {
     OPTION_REG = 0x00;
     OPTION_REGbits.PS = 0b111; // 256 prescaler
     OPTION_REGbits.nRBPU = 1;
-
-    INTCONbits.GIE = 1; //enable interrupts
-    INTCONbits.T0IE = 1; //enable overflow interrupt
 
     TMR0 = 0;
 }
@@ -51,10 +34,28 @@ int get_pin(int id) {
     return (PORTB >> id) & 1;
 }
 
+void set_signal(int b) {
+    PORTAbits.RA0 = b;
+}
+
 void main(void) {
     init();
 
+    unsigned counters[10];
+    int signal_flag = 0;
+    int signal_timer = 0;
+
     while (1) {
+        if (T0IF) {
+            T0IF = 0; //Resetting the overflow flag
+            for (int i = 0; i < 10; i++) {
+                counters[i]++;
+            }
+            if (signal_flag) {
+                signal_timer++;
+            }
+        }
+
         for (int i = 0; i < 10; i++) {
             if (!get_pin(i)) {
                 counters[i] = 0;
@@ -63,15 +64,13 @@ void main(void) {
                 counters[i] = 0;
                 signal_flag = 1;
                 signal_timer = 0;
-                PORTAbits.RA0 = 1;
+                set_signal(1);
             }
         }
-        if (signal_flag) {
-            if (signal_timer >= DELAY_SIGNAL) {
-                signal_flag = 0;
-                signal_timer = 0;
-                PORTAbits.RA0 = 0;
-            }
+        if (signal_flag && signal_timer >= DELAY_SIGNAL) {
+            signal_flag = 0;
+            signal_timer = 0;
+            set_signal(0);
         }
     }
     return;
